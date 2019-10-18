3e8fde05a72909ee49295ba7bc7895de752003d2
techsithgit <hemilp@tuplecorp.com>
2019-10-18 08:44:46 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import User from './pages/User';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to={
+                {
+                  pathname: "/about", 
+                  state: {
+                    from:"root"
+                  }
+                }
+              }>About</Link>
+            </li>
+            <li>
+              <Link to="/user/john/johnson">User</Link>
+            </li>
+          </ul>
+          <div>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/user/:firstname/:lastname" component={User}/>
+            </Switch>
+          </div>
+        </header>
+      </div>
+    </Router>
   );
 }
 
